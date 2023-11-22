26a0b1a5221667969eecb54275ff7ed9eb3b5cc1
Mustafa Ali <mustafaali8254@gmail.com>
2023-11-22 17:27:56 +0500

    file updated

---
+++ index.js
@@ -0,0 +1,24 @@
+
+ function showMenu(){
+  document.getElementById("ul").style.display = "flex" ;
+}
+
+function hideMenu() {
+  document.getElementById("ul").style.display = "none" ;
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
